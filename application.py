1c30f267267beef7ced21520d6b4c65f6e72f8a8
Pablo Rios <pariosur@gmail.com>
2022-10-30 17:57:52 +0300

    adding db

--- application.py
+++ application.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
 
 application = Flask(__name__)
+app = application
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(application)
+app.app_context().push()
+
+class Todo(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    completed = db.Column(db.Integer, default = 0)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
 
 @application.route("/")
 def hello_world():
-    return 'Unilaser Hair Removal Count App'
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    application.run(debug=True)
