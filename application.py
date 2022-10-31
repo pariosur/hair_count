218ade31d3b4c8e65e0033a4e2a7eb68b8480652
Pablo Rios <pariosur@gmail.com>
2022-10-31 17:04:37 +0300

    changing application name

--- application.py
+++ application.py
@@ -40,7 +40,7 @@ def upload_image():
     return render_template('index.html', form=form, file_url=file_url)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
 
 
 
