dc0740e46e9c763d7b4113cfc2498c66cb7a1174
Pablo Rios <pariosur@gmail.com>
2022-10-31 15:36:00 +0300

    changing template

--- application.py
+++ application.py
@@ -1,68 +1,135 @@
-from datetime import datetime
-from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+
+#app.py
+from email.mime import application
+from flask import Flask, flash, request, redirect, url_for, render_template
+import urllib.request
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads/'
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'JPG'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-application = Flask(__name__)
-app = application
-# app.app_context().push()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-class Todo(db.Model):
-
-    id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(200), nullable = False)
-    completed = db.Column(db.Integer, default = 0)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
-
-
-
-@app.route("/", methods=['POST', 'GET'])
-def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
-
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
-
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        return render_template('index.html', filename=filename)
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that task'
-
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-
-def update(id):
-    task = Todo.query.get_or_404(id)
-    if request.method =='POST':
-        task.content = request.form['content']
-        try:
-            db.session.commit()
-            return redirect ('/')
-        except:
-            return 'There was an issue updating your task'
-    else:
-        return render_template('update.html',task=task)
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################################################
+################################################################
+################################################################
+
+# from datetime import datetime
+# from flask import Flask, render_template, url_for, request, redirect
+# from flask_sqlalchemy import SQLAlchemy
+# from datetime import datetime
+
+
+# application = Flask(__name__)
+# app = application
+# # app.app_context().push()
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# db = SQLAlchemy(app)
+
+# class Todo(db.Model):
+
+#     id = db.Column(db.Integer, primary_key = True)
+#     content = db.Column(db.String(200), nullable = False)
+#     completed = db.Column(db.Integer, default = 0)
+#     date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+#     def __repr__(self):
+#         return '<Task %r>' % self.id
+
+
+# @app.route("/", methods=['POST', 'GET'])
+# def index():
+#     if request.method == 'POST':
+#         task_content = request.form['content']
+#         new_task = Todo(content=task_content)
+
+#         try:
+#             db.session.add(new_task)
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return 'There was an issue adding your task'
+
+#     else:
+#         tasks = Todo.query.order_by(Todo.date_created).all()
+#         return render_template('index.html', tasks=tasks)
+
+# @app.route('/delete/<int:id>')
+# def delete(id):
+#     task_to_delete = Todo.query.get_or_404(id)
+
+#     try:
+#         db.session.delete(task_to_delete)
+#         db.session.commit()
+#         return redirect('/')
+#     except:
+#         return 'There was a problem deleting that task'
+
+# @app.route('/update/<int:id>', methods=['GET', 'POST'])
+
+# def update(id):
+#     task = Todo.query.get_or_404(id)
+#     if request.method =='POST':
+#         task.content = request.form['content']
+#         try:
+#             db.session.commit()
+#             return redirect ('/')
+#         except:
+#             return 'There was an issue updating your task'
+#     else:
+#         return render_template('update.html',task=task)
+
+# if __name__ == "__main__":
+#     application.run(debug=True)
